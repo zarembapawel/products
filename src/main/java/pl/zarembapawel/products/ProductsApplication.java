0b945bfc2abe5eeefebcdca4b793e0cce7df57ff
zarembapawel <zarembapawel95@gmail.com>
2023-08-21 17:39:04 +0200

    Initial commit

---
+++ src/main/java/pl/zarembapawel/products/ProductsApplication.java
@@ -0,0 +1,13 @@
+package pl.zarembapawel.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "pl.zarembapawel.customers")
+public class ProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApplication.class, args);
+	}
+
+}
